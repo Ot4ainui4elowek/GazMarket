baadb66c07bb3181783892c0e450efc1814f5218
Bogdan <bogdan232137@list.ru>
2025-10-06 18:21:18 +0300

    moove main.tsx and styles to infrastructure

--- src/infrastructure/main.tsx
+++ src/infrastructure/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
 import './index.scss'
-import { AppRouter } from './infrastructure/router/router.tsx'
-import { store } from './shared/store/store.ts'
+import { AppRouter } from './router/router.tsx'
+import { store } from '../shared/store/store.ts'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
